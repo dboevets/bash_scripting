5ea4b9ae8b64593b2367fdef1811ef57a762f3c5
dboevets <dboevets@zeo.ua>
2019-09-25 09:19:49 +0300

    just test button Fetch

--- backup_file.sh
+++ backup_file.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# just test button Fetch
 function backup_file() {
   if [ -f $1 ]
   then
     back="/tmp/$(basename ${1}).$(date +%F).$$"
+    # /tmp/hosts.2019-09-25.26575
     echo "Backing up $1 to ${back}"
     cp $1 $back
   fi
