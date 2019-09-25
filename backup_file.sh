ce107026d2c9bce79bb5fdcb3e3386b4983bea23
dboevets <dboevets@zeo.ua>
2019-09-25 08:54:35 +0300

    all must work!

--- backup_file.sh
+++ backup_file.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+function backup_file() {
+  if [ -f $1 ]
+  then
+    back="/tmp/$(basename ${1}).$(date +%F).$$"
+    echo "Backing up $1 to ${back}"
+    cp $1 $back
+  fi
+}
+backup_file /etc/hosts
+if [[ $? -eq 0 ]]; then
+  echo "Backup succeeded!"
+fi
